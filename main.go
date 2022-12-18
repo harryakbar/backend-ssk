64b99f661238232292c8d8ad27f1b0e96089cf3e
harryakbar <harryakbaram@gmail.com>
2022-12-18 15:53:08 +0800

    Test gcloud trigger

--- main.go
+++ main.go
@@ -82,6 +82,7 @@ func main() {
 			return
 		}
 
+		
 		w.Header().Set("Access-Control-Allow-Origin", "*")
 		w.Header().Set("Content-Type", "application/json; charset=utf-8")
 		json.NewEncoder(w).Encode(data)
